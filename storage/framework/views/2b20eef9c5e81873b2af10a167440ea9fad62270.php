9728af54a5f2cae9cfec258e747ed7c1f6f7016d
nobledsmarts <nobledsmarts@gmail.com>
2024-09-10 15:27:43 +0100

    remove activation script

---
+++ storage/framework/views/2b20eef9c5e81873b2af10a167440ea9fad62270.php
@@ -0,0 +1,28 @@
+<!-- TOPBAR | TOPBAR-LOGGEDIN -->
+<section id="topbar" class="topbar-loggedin">
+    <div class="<?php echo e(Request::routeIs('user*') ? 'container-fluid' : 'container'); ?>  ">
+        <div class="row">
+            <div class="col-md-6">
+                <div
+                    class="topbar-content d-flex align-items-center justify-content-between justify-content-md-end">
+                    <div class="language-wrapper">
+                        <div class="control-plugin">
+                            <div class="language"
+                                 data-input-name="country3"
+                                 data-selected-country="<?php echo e(app()->getLocale() ? : 'US'); ?>"
+                                 data-button-size="btn-sm"
+                                 data-button-type="btn-info"
+                                 data-scrollable="true"
+                                 data-scrollable-height="250px"
+                                 data-countries='<?php echo e($languages); ?>'>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- /TOPBAR | TOPBAR-LOGGEDIN -->
+<?php /**PATH C:\laragon\www\elmshrty\resources\views/themes/deepblue/partials/topbar-auth.blade.php ENDPATH**/ ?>
